8412d619a4d2951715acbbf80de099c11ca25c5c
walchko <walchko@users.noreply.github.com>
2024-11-03 13:30:36 -0500

    update

--- src/serialcomm/serial_port.hpp
+++ src/serialcomm/serial_port.hpp
@@ -33,16 +33,28 @@ SOFTWARE.
 #include <termios.h>   // serial
 #include <unistd.h>    // write(), read(), close()
 
-// class SerialError : public std::exception {
-// public:
-//   SerialError(const std::string &s) : msg(s) {}
-//   SerialError() : msg("Serial Error") {}
-//   const char *what() const throw() { return msg.c_str(); }
 
-// protected:
-//   std::string msg;
-// };
-
+// These datarates should already be defined
+// B0
+// B50
+// B75
+// B110
+// B134
+// B150
+// B200
+// B300
+// B600
+// B1200
+// B1800
+// B2400
+// B4800
+// B9600
+// B19200
+// B38400
+// B57600
+// B115200
+// B230400
+//
 // macos is broken!!!
 // define some higher, missing data rates
 #ifndef B1000000
@@ -54,67 +66,105 @@ SOFTWARE.
   #define B4000000 0010017
 #endif
 
-// typedef struct {
-//     uint8_t id;
-//     uint8_t error;
-//     std::vector<uint8_t> params;
-// } status_t;
+// O_RDWR     - Read/Write access to serial port
+// O_NOCTTY   - No terminal will control the process
+// O_SYNC     - data is written synchronously
+// O_NONBLOCK - will always return immediately and will not wait
+// O_NDELAY   - tells UNIX to ignore state of DCD signals, same as O_NONBLOCK
+constexpr int O_FLAGS_NONBLOCKING = O_RDWR | O_NOCTTY | O_NONBLOCK;
+constexpr int O_FLAGS_BLOCKING = O_RDWR | O_NOCTTY;
 
-// enum class SerialError: int {
-//   OK = 0,
-//   ERROR_OPEN,
-//   ERROR_FLUSH,
-//   ERROR_IN_FLUSH,
-//   ERROR_OUT_FLUSH,
-//   ERROR_SET,
-//   ERROR_SET_DTR,
-//   ERROR_SET_RTS,
-//   ERROR_AVAILABLE,
-//   ERROR_TIMEOUT,
-//   ERROR_PEAK
+// pass a struct into open() to give more options
+// maybe this is a good idea or not ... not sure
+// struct port_info_t {
+//   std::string port; // /dev/tty.usbmodemXXXX
+//   int baud{115200}; // 9600, 115200, 1000000, etc
+//   int o_flags{O_FLAGS_NONBLOCKING}; // flags for open
+
+//   // IF using blocking flags, you can put a timeout or char read
+//   // to override, otherwise these are ignored
+//   int timeout{0}; // 1 = 0.1 sec (100 msec), not a fine resolution
+//   // int min_chars{0}; // number of chars to receive before return, don't use
 // };
-
-// inline
-// SerialError guard(int err, SerialError val) {
-//   if (err < 0) return val;
-//     // throw SerialError(msg + getError());
-//   return SerialError::OK;
-// }
 
 /**
  * Serial port
  */
-class Stream {
+class SerialPort {
 public:
-  Stream() : fd(0) {}
-  ~Stream() { this->close(); }
+  SerialPort(): fd(0) {}
+  SerialPort(const SerialPort &)  = delete; // copy
+  SerialPort(const SerialPort &&) = delete; // move
+  ~SerialPort() { this->close(); }
 
-  void begin(int i) {} // does nothing
+  // void begin(int i) {} // does nothing
 
-  bool open(const std::string &port, int speed) {
+  bool open(const std::string &port, int speed, int flags=O_FLAGS_NONBLOCKING, int timeout=0) {
 
     // O_RDWR means that the port is opened for both reading and writing.
     // O_NOCTTY means that no terminal will control the process opening the
     // serial port.
-    fd = ::open(port.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
+    fd = ::open(port.c_str(), flags);
     if (fd < 0) {
       fd = 0;
       return false;
     }
 
+    int baud{0};
+
+    // honestly I can't image any thing slower than GPS at 9600
+    switch(speed) {
+      case 2400:
+        baud = B2400;
+        break;
+      case 4800:
+        baud = B4800;
+        break;
+      case 9600:
+        baud = B9600;
+        break;
+      case 19200:
+        baud = B19200;
+        break;
+      case 38400:
+        baud = B38400;
+        break;
+      case 57600:
+        baud = B57600;
+        break;
+      case 115200:
+        baud = B115200;
+        break;
+      case 230400:
+        baud = B230400;
+        break;
+      case 1000000:
+        baud = B1000000;
+        break;
+      case 2000000:
+        baud = B2000000;
+        break;
+      default:
+        return false;
+    }
+
     struct termios t;
     memset(&t, 0, sizeof(t)); // clear struct for new port settings
 
-    t.c_cflag = speed | CS8 | CLOCAL | CREAD;
+    t.c_cflag = baud | CS8 | CLOCAL | CREAD;
     t.c_iflag = IGNPAR;
-    t.c_oflag = 0;
+    t.c_oflag = 0; // already cleared by memset
     t.c_lflag = 0;
-
-    // VTIME: 10th of second, 1 = 0.1 sec, time to block
-    // before return
     t.c_cc[VTIME] = 0;
-    // VMIN: min number of characters before return
     t.c_cc[VMIN] = 0;
+    // if blocking, we can set a timeout
+    if (!(flags & O_NONBLOCK)) {
+      // VTIME: 10th of second, 1 = 0.1 sec, time to block
+      // before return
+      t.c_cc[VTIME] = timeout;
+      // VMIN: min number of characters before return
+      t.c_cc[VMIN] = 0;
+    }
 
     // activate the settings for the port
     if (tcsetattr(fd, TCSANOW, &t) < 0) return false;
@@ -122,64 +172,60 @@ public:
     return true;
   }
 
-  bool open(int d) {
-    fd = d;
-    return true;
-  }
+  // bool open(int d) {
+  //   fd = d;
+  //   return true;
+  // }
 
   void close() {
     if (fd > 0) ::close(fd);
   }
 
   int write(const uint8_t byte) {
-    // int ret = guard(::write(fd, (void *)&byte, 1), "write(byte): ");
     return ::write(fd, (void *)&byte, 1);
   }
   int write(const std::string &s) {
-    // int ret =
-    //     guard(::write(fd, (void *)s.c_str(), s.size()), "write(string): ");
     return ::write(fd, (void *)s.c_str(), s.size());
   }
   int write(const void *buffer, int size) {
-    // int ret = guard(::write(fd, buffer, size), "write(buffer): ");
     return ::write(fd, buffer, size);
   }
 
-  inline void print(const std::string &s) { write(s); }
-  inline void println(const std::string &s) { write(s + "\n"); }
+  // inline void print(const std::string &s) { write(s); }
+  // inline void println(const std::string &s) { write(s + "\n"); }
 
-  int read() {
-    // std::string ret;
-    int c;
-    int num = ::read(fd, (void *)&c, 1);
-    // int num = guard(::read(fd, (void *)&c, 1), "read(): ");
-    if (num <= 0) c = -1; // timeout if non-blocking
-    return c;
-  }
+  // int read() {
+  //   // std::string ret;
+  //   int c;
+  //   int num = ::read(fd, (void *)&c, 1);
+  //   if (num <= 0) c = -1; // timeout if non-blocking
+  //   return c;
+  // }
 
-  std::string readString() {
-    std::string ret;
-    char c;
-    int num = available();
-    for (int i = 0; i < num; ++i) {
-      int num =
-          ::read(fd, (void *)&c,
-                 1); // guard(::read(fd, (void *)&c, 1), "readString(): ");
-      if (num < 0) break;
-      ret.push_back(c);
-    }
-    return ret;
-  }
+  // std::string readString() {
+  //   std::string ret;
+  //   char c;
+  //   int err;
+  //   int num = available();
+  //   for (int i = 0; i < num; ++i) {
+  //     err = ::read(fd, (void *)&c, 1);
+  //     if (err < 0) break;
+  //     ret.push_back(c);
+  //   }
+  //   return ret;
+  // }
+
+  inline
+  int read(uint8_t *buf, int size) { return readBytes(buf, size); }
 
   int readBytes(uint8_t *buf, int size) {
     int num = 0;
     memset(buf, 0, size);
+    int err;
 
     while (num < size) {
-      int err = ::read(fd, (void *)&buf[num],
-                       size - num); // guard(::read(fd, (void *)&buf[num], size
-                                    // - num), "readBytes(): ");
-      if (err < 0) return err;
+      err = ::read(fd, (void *)&buf[num], size - num);
+      if (err < 0) return err; // why isn't this num?
       num += err;
     }
 
@@ -196,6 +242,7 @@ public:
     return true;
   }
 
+  // flush both input/output
   bool flush() {
     if (tcflush(fd, TCIOFLUSH) < 0) return false;
     return true;
@@ -215,10 +262,11 @@ public:
     return true;
   }
 
-  bool setTimeout(int time) { /* FIXME */
-    return false;
-  }
+  // bool setTimeout(int time) { /* FIXME */
+  //   return false;
+  // }
 
+  // FIXME: does this work on both macOS and Linux?
   int available() {
     int bytes;
     // guard(ioctl(fd, FIONREAD, &bytes), "available(): ");
@@ -226,7 +274,7 @@ public:
     return bytes;
   }
 
-  inline int peek() { return -1; } // FIXME: can I do thiis?
+  // inline int peek() { return -1; } // FIXME: can I do thiis?
   // int peek() {
   //   int c = fgetc(fd);
   //   ungetc(c, fd);
@@ -238,9 +286,9 @@ public:
   inline const bool is_open() const { return (fd > 0) ? true : false; }
 
 protected:
-  int fd;
+  int fd{0};
   // std::string error_string; // is something like this better than throw/catch
   // error?
 };
 
-class SerialPort : public Stream {};
+// class SerialPort : public Stream {};
